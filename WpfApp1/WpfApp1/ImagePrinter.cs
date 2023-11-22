0a5acadd2df61f84da02d9a8ae2d97ad8fa28b1d
kei ito <ito.kei@jvckenwood.com>
2023-11-22 14:30:30 +0900

    GDIオブジェクト経由で実装完了。低速のためボツ

---
+++ WpfApp1/WpfApp1/ImagePrinter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp1
+{
+    internal class ImagePrinter
+    {
+        private Bitmap _bitmap = new Bitmap(12800, 12800);
+
+        private System.Windows.Controls.Image _imageControl;
+
+        public ImagePrinter(System.Windows.Controls.Image imageControl)
+        {
+            _imageControl = imageControl;
+        }
+
+        public void Print()
+        {
+            Bitmap image = GetBitmap();
+            IntPtr hbitmap = image.GetHbitmap();
+
+            _imageControl.Source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hbitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            
+            DeleteObject(hbitmap);
+        }
+
+        private Bitmap GetBitmap()
+        {
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics g = Graphics.FromImage(_bitmap);
+
+            //描画する
+            var pen = new Pen(Color.Black, 10);
+            for (int i = 0; i < 10000; i++)
+            {
+                // 描画位置
+                var point = GetRandomPoint();
+                //長方形を描く
+                g.DrawRectangle(pen, point.X, point.Y, 2, 2);
+            }
+
+            //リソースを解放する
+            g.Dispose();
+
+            return _bitmap;
+        }
+
+        private static System.Drawing.Point GetRandomPoint()
+        {
+            Random r1 = new System.Random();
+            return new(r1.Next(0, 639) * 20, r1.Next(0, 639) * 20);
+        }
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject); // gdi32.dllのDeleteObjectメソッドの使用を宣言する。
+    }
+}
--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -21,34 +21,18 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ImagePrinter _printer;
+
         public MainWindow()
         {
             InitializeComponent();
 
-
-
-            //200x100サイズのImageオブジェクトを作成する
-            Bitmap img = new Bitmap(600, 300);
-
-            //ImageオブジェクトのGraphicsオブジェクトを作成する
-            Graphics g = Graphics.FromImage(img);
-
-            //全体を黒で塗りつぶす
-            g.FillRectangle(System.Drawing.Brushes.Black, g.VisibleClipBounds);
-            //黄色い扇形を描画する
-            g.DrawPie(Pens.Yellow, 60, 10, 80, 80, 30, 300);
-
-            //リソースを解放する
-            g.Dispose();
-
-            //作成した画像を表示する
-            IntPtr hbitmap = img.GetHbitmap();
-            image.Source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hbitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
-            DeleteObject(hbitmap);
+            _printer = new ImagePrinter(image);
         }
 
-        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
-        public static extern bool DeleteObject(IntPtr hObject); // gdi32.dllのDeleteObjectメソッドの使用を宣言する。
-
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _printer.Print();
+        }
     }
 }
