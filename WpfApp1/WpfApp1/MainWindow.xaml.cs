8311a2076675975fdb7f77d86b679803bcc18907
kei ito <ito.kei@jvckenwood.com>
2023-11-22 13:13:46 +0900

    生成したBitmapを画面に表示

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,31 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+
+
+
+            //200x100サイズのImageオブジェクトを作成する
+            Bitmap img = new Bitmap(600, 300);
+
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics g = Graphics.FromImage(img);
+
+            //全体を黒で塗りつぶす
+            g.FillRectangle(System.Drawing.Brushes.Black, g.VisibleClipBounds);
+            //黄色い扇形を描画する
+            g.DrawPie(Pens.Yellow, 60, 10, 80, 80, 30, 300);
+
+            //リソースを解放する
+            g.Dispose();
+
+            //作成した画像を表示する
+            IntPtr hbitmap = img.GetHbitmap();
+            image.Source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hbitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            DeleteObject(hbitmap);
         }
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject); // gdi32.dllのDeleteObjectメソッドの使用を宣言する。
+
     }
 }
